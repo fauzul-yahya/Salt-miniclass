9adb208b5b2b5b6c1ce94c3a13f197e9ff3804a2
fauzul-yahya <fauzulyahya@gmail.com>
2022-11-09 20:11:15 +0700

    miniclass

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Copyright &#169; 2022 Created by Fauzul Yahya </p>
+    </footer>
+  );
+};
+
+export default Footer;
